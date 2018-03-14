d64bb8349f4c4262f127d9044513eb4d2f90b873
hongquan95 <hongquan95.tran@gmail.com>
2018-03-14 07:58:11 +0700

    do user_course

---
+++ app/controllers/user_courses_controller.rb
@@ -0,0 +1,4 @@
+class UserCoursesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'user_courses/index'
+
   root "static_pages#home"
   get "/help", to: "static_pages#help"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/signup", to: "users#new"
-  resources :users
+  resources :users do
+    resources :user_courses, as: "courses"
+  end
 end
