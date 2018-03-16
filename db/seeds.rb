cc68605ec457152001ef70b7dfb024a83ec70fbc
hongquan95 <hongquan95.tran@gmail.com>
2018-03-16 09:21:52 +0700

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,14 @@ end
                password_confirmation: password,
                role: 2)
 end
+
+20.times do |n|
+  name  = Faker::Name.name
+  email = "trainee-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               role: 2)
+end
